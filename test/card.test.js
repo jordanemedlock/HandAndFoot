64eeb985fe0832349e81787cc1fe55b5fc73dfcb
Jordan Medlock <jordan@innovo.net>
2019-08-07 09:58:21 -0700

    refactored card.test.js

--- test/card.test.js
+++ test/card.test.js
@@ -1,74 +1,98 @@
 const {Card, Rank, Suit, Color} = require('../app/models/card.js')
 
-test('equivalent cards should equal eachother and oposite', () => {
-	var card1 = new Card(Rank.ACE, Suit.HEARTS);
-	var card2 = new Card(Rank.ACE, Suit.HEARTS);
-	expect(card1.equals(card2)).toBe(true);
-	var card3 = new Card(Rank.ACE, Suit.SPADES);
-	expect(card1.equals(card3)).toBe(false);
-	var card4 = new Card(Rank.TWO, Suit.HEARTS);
-	expect(card1.equals(card4)).toBe(false);
+const AH1 = new Card(Rank.ACE, Suit.HEARTS);
+const AH2 = new Card(Rank.ACE, Suit.HEARTS);
+const AS = new Card(Rank.ACE, Suit.SPADES);
+const TH = new Card(Rank.TWO, Suit.HEARTS);
+const TS = new Card(Rank.TWO, Suit.SPADES);
+const FS = new Card(Rank.FIVE, Suit.SPADES);
+const RJ = Card.RED_JOKER;
+const ThH = new Card(Rank.THREE, Suit.HEARTS);
+const ThS = new Card(Rank.THREE, Suit.SPADES);
+const SH = new Card(Rank.SEVEN, Suit.HEARTS);
+const EH = new Card(Rank.EIGHT, Suit.HEARTS);
+const TeH = new Card(Rank.TEN, Suit.HEARTS);
+const QH = new Card(Rank.QUEEN, Suit.HEARTS);
+
+
+describe('Card', () => {
+
+	test('equals compares cards', () => {
+		expect(AH1.equals(AH2)).toBeTruthy();
+		expect(AH1.equals(AS)).toBeFalsy();
+		expect(AH1.equals(TH)).toBeFalsy();
+	})
+
+	test('getRank returns rank', () => {
+		expect(AH1.getRank().equals(Rank.ACE)).toBeTruthy();
+	})
+
+	test('getSuit return suit', () => {
+		expect(AH1.getSuit().equals(Suit.HEARTS)).toBeTruthy();
+	})
+
+	test.each([[TH, true], [TS, true], [FS, false], [RJ, true]])(
+		'test isWild', 
+		(card, expected) => {
+			expect(card.isWild()).toBe(expected);
+		}
+	)
+
+	test.each([
+			[TH, false], [RJ, false], [ThH, false],
+			[ThS, false], [FS, true], [AH1, true]
+		])(
+		'test isNormalCard', 
+		(card, expected) => {
+			expect(card.isNormalCard()).toBe(expected);
+		}
+	)
+
+	test.each([
+			[FS, 5], [SH, 5], [EH, 10],
+			[TeH, 10], [QH, 10], [AH1, 20],
+			[TH, 20], [RJ, 50], [ThS, 50],
+			[ThH, 100]
+		])(
+		'test points', 
+		(card, expected) => {
+			expect(card.points()).toBe(expected);
+		}
+	)
+
 })
 
-test('test getRank and getSuit', () => {
-	var card = new Card(Rank.JACK, Suit.DIAMONDS);
-	expect(card.getRank().equals(Rank.JACK)).toBe(true);
-	expect(card.getSuit().equals(Suit.DIAMONDS)).toBe(true);
+describe('Color', () => {
+	test('equals should work', () => {
+		expect(Color.RED.equals(Color.RED)).toBe(true);
+		expect(Color.RED.equals(Color.BLACK)).toBe(false);
+		expect(Color.BLACK.equals(Color.BLACK)).toBe(true);
+		expect(Color.BLACK.equals(Color.RED)).toBe(false);
+	})
 })
 
-test('color equals should work', () => {
-	expect(Color.RED.equals(Color.RED)).toBe(true);
-	expect(Color.RED.equals(Color.BLACK)).toBe(false);
-	expect(Color.BLACK.equals(Color.BLACK)).toBe(true);
-	expect(Color.BLACK.equals(Color.RED)).toBe(false);
+describe('Suit', () => {
+	test('suit equals should work', () => {
+		expect(Suit.HEARTS.equals(Suit.HEARTS)).toBe(true);
+		expect(Suit.HEARTS.equals(Suit.DIAMONDS)).toBe(false);
+		expect(Suit.CLUBS.equals(Suit.SPADES)).toBe(false);
+		expect(Suit.CLUBS.equals(Suit.CLUBS)).toBe(true);
+	})
+
+	test('getColor should return color', () => {
+		expect(Suit.HEARTS.getColor().equals(Color.RED)).toBe(true);
+		expect(Suit.DIAMONDS.getColor().equals(Color.RED)).toBe(true);
+		expect(Suit.SPADES.getColor().equals(Color.BLACK)).toBe(true);
+		expect(Suit.CLUBS.getColor().equals(Color.BLACK)).toBe(true);
+	})
+
 })
 
-test('suit equals should work', () => {
-	expect(Suit.HEARTS.equals(Suit.HEARTS)).toBe(true);
-	expect(Suit.HEARTS.equals(Suit.DIAMONDS)).toBe(false);
-	expect(Suit.CLUBS.equals(Suit.SPADES)).toBe(false);
-	expect(Suit.CLUBS.equals(Suit.CLUBS)).toBe(true);
-})
-
-test('rank equals should work', () => {
-	expect(Rank.ACE.equals(Rank.ACE)).toBe(true);
-	expect(Rank.ACE.equals(Rank.TWO)).toBe(false);
-	expect(Rank.THREE.equals(Rank.KING)).toBe(false);
-	expect(Rank.THREE.equals(Rank.THREE)).toBe(true);
-})
-
-test('test getColor', () => {
-	expect(Suit.HEARTS.getColor().equals(Color.RED)).toBe(true);
-	expect(Suit.DIAMONDS.getColor().equals(Color.RED)).toBe(true);
-	expect(Suit.SPADES.getColor().equals(Color.BLACK)).toBe(true);
-	expect(Suit.CLUBS.getColor().equals(Color.BLACK)).toBe(true);
-})
-
-test('test isWild', () => {
-	expect((new Card(Rank.TWO, Suit.HEARTS)).isWild()).toBe(true);
-	expect((new Card(Rank.TWO, Suit.SPADES)).isWild()).toBe(true);
-	expect((new Card(Rank.FIVE, Suit.SPADES)).isWild()).toBe(false);
-	expect(Card.RED_JOKER.isWild()).toBe(true);
-})
-
-test('test isNormalCard', () => {
-	expect((new Card(Rank.TWO, Suit.HEARTS)).isNormalCard()).toBe(false);
-	expect(Card.RED_JOKER.isNormalCard()).toBe(false);
-	expect((new Card(Rank.THREE, Suit.HEARTS)).isNormalCard()).toBe(false);
-	expect((new Card(Rank.THREE, Suit.SPADES)).isNormalCard()).toBe(false);
-	expect((new Card(Rank.FIVE, Suit.SPADES)).isNormalCard()).toBe(true);
-	expect((new Card(Rank.ACE, Suit.HEARTS)).isNormalCard()).toBe(true);
-})
-
-test('test points', () => {
-	expect((new Card(Rank.FIVE, Suit.HEARTS)).points()).toBe(5);
-	expect((new Card(Rank.SEVEN, Suit.HEARTS)).points()).toBe(5);
-	expect((new Card(Rank.EIGHT, Suit.HEARTS)).points()).toBe(10);
-	expect((new Card(Rank.TEN, Suit.HEARTS)).points()).toBe(10);
-	expect((new Card(Rank.QUEEN, Suit.HEARTS)).points()).toBe(10);
-	expect((new Card(Rank.ACE, Suit.HEARTS)).points()).toBe(20);
-	expect((new Card(Rank.TWO, Suit.HEARTS)).points()).toBe(20);
-	expect((new Card(Rank.JOKER, Suit.HEARTS)).points()).toBe(50);
-	expect((new Card(Rank.THREE, Suit.HEARTS)).points()).toBe(100);
-	expect((new Card(Rank.THREE, Suit.CLUBS)).points()).toBe(50);
+describe('Rank', () => {
+	test('rank equals should work', () => {
+		expect(Rank.ACE.equals(Rank.ACE)).toBe(true);
+		expect(Rank.ACE.equals(Rank.TWO)).toBe(false);
+		expect(Rank.THREE.equals(Rank.KING)).toBe(false);
+		expect(Rank.THREE.equals(Rank.THREE)).toBe(true);
+	})
 })
