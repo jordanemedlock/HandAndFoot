e5b9ff9650d0841121314878f59806b8b0a8e4d6
Jordan Medlock <jordan@innovo.net>
2019-08-07 11:02:13 -0700

    refactored meld.test.js

--- test/meld.test.js
+++ test/meld.test.js
@@ -1,165 +1,125 @@
 const {Meld} = require('../app/models/meld.js')
 const {Card, Rank, Suit} = require('../app/models/card.js')
 
-test('creating a simple meld and validating it', () => {
-	var rank = Rank.FIVE;
-	var cards = [
-		new Card(Rank.FIVE, Suit.DIAMONDS), 
-		new Card(Rank.FIVE, Suit.HEARTS),
-		new Card(Rank.FIVE, Suit.CLUBS)
-	];
-	var meld = new Meld(rank, cards);
-	expect(meld.validate()).toBe(true);
+const AH1 = new Card(Rank.ACE, Suit.HEARTS);
+const AH2 = new Card(Rank.ACE, Suit.HEARTS);
+const AS = new Card(Rank.ACE, Suit.SPADES);
+const TH = new Card(Rank.TWO, Suit.HEARTS);
+const TS = new Card(Rank.TWO, Suit.SPADES);
+const FH = new Card(Rank.FIVE, Suit.HEARTS);
+const FD = new Card(Rank.FIVE, Suit.DIAMONDS);
+const FS = new Card(Rank.FIVE, Suit.SPADES);
+const FC = new Card(Rank.FIVE, Suit.CLUBS);
+const RJ = Card.RED_JOKER;
+const BJ = Card.BLACK_JOKER;
+const ThH = new Card(Rank.THREE, Suit.HEARTS);
+const ThS = new Card(Rank.THREE, Suit.SPADES);
+const ThC = new Card(Rank.THREE, Suit.CLUBS);
+const ThD = new Card(Rank.THREE, Suit.DIAMONDS);
+const SH = new Card(Rank.SEVEN, Suit.HEARTS);
+const EH = new Card(Rank.EIGHT, Suit.HEARTS);
+const TeH = new Card(Rank.TEN, Suit.HEARTS);
+const QH = new Card(Rank.QUEEN, Suit.HEARTS);
+
+const melds = [
+	[Rank.FIVE, [FH, FD, FS], 	true, 	15],
+	[Rank.FIVE, [FH, FD, TS], 	true, 	30],
+	[Rank.FIVE, [FH, FD], 			false, 	null],
+	[Rank.FIVE, [FH, FD, SH], 	false,  null],
+
+	[Rank.EIGHT, [EH, TS, RJ], 	false, 	null],
+	[Rank.TWO, [TS, RJ, BJ], 		true,		120],
+	[Rank.THREE, [ThH], 				true,		100],
+	[Rank.THREE, [ThS, ThC, ThS], false, 	null],
+	[Rank.THREE, [ThH, ThH, ThD], true, 	300],
+	[Rank.THREE, [ThH, ThH, TS], 	false,	null]
+]
+
+describe('Meld', () => {
+	test.each(melds)(
+		'validates or invalidates melds', 
+		(rank, cards, expected, points) => {
+			expect((new Meld(rank, cards)).validate()).toBe(expected);
+		}
+	)
+
+	test('adding cards to a created meld', () => {
+		var rank = Rank.FIVE;
+		var cards = [
+			new Card(Rank.FIVE, Suit.HEARTS), 
+			new Card(Rank.FIVE, Suit.DIAMONDS),
+			new Card(Rank.FIVE, Suit.HEARTS),
+		];
+		var meld = new Meld(rank, cards);
+		meld.addCard(new Card(Rank.FIVE, Suit.CLUBS));
+		expect(meld.validate()).toBe(true);
+		expect(meld.size()).toBe(4);
+	})
+
+	test('test isBook and getBookKind on red canasta', () => {
+		var meld = new Meld(Rank.FIVE, [
+			new Card(Rank.FIVE, Suit.HEARTS),
+			new Card(Rank.FIVE, Suit.DIAMONDS),
+			new Card(Rank.FIVE, Suit.CLUBS)
+		])
+		expect(meld.validate()).toBe(true);
+		expect(meld.isBook()).toBe(false);
+		expect(meld.getBookKind()).toBe(null);
+
+		meld.addCards([
+			new Card(Rank.FIVE, Suit.HEARTS),
+			new Card(Rank.FIVE, Suit.SPADES),
+			new Card(Rank.FIVE, Suit.DIAMONDS),
+			new Card(Rank.FIVE, Suit.HEARTS)
+		])
+		expect(meld.validate()).toBe(true);
+		expect(meld.isBook()).toBe(true);
+		expect(meld.getBookKind()).toBe('PURE');
+
+	})
+
+	test('test isBook and getBookKind on black canasta', () => {
+		var meld = new Meld(Rank.FIVE, [
+			new Card(Rank.FIVE, Suit.HEARTS),
+			new Card(Rank.FIVE, Suit.DIAMONDS),
+			new Card(Rank.FIVE, Suit.CLUBS),
+			Card.RED_JOKER,
+			new Card(Rank.FIVE, Suit.SPADES),
+			new Card(Rank.TWO, Suit.DIAMONDS),
+			new Card(Rank.FIVE, Suit.HEARTS)
+		])
+		expect(meld.validate()).toBe(true);
+		expect(meld.isBook()).toBe(true);
+		expect(meld.getBookKind()).toBe('DIRTY');
+
+	})
+
+	test('test isBook and getBookKind on wild canasta', () => {
+		var meld = new Meld(Rank.JOKER, [
+			new Card(Rank.TWO, Suit.HEARTS),
+			new Card(Rank.TWO, Suit.DIAMONDS),
+			new Card(Rank.TWO, Suit.CLUBS),
+			new Card(Rank.TWO, Suit.HEARTS),
+			Card.RED_JOKER,
+			Card.BLACK_JOKER,
+			Card.RED_JOKER
+		])
+		expect(meld.validate()).toBe(true);
+		expect(meld.isBook()).toBe(true);
+		expect(meld.getBookKind()).toBe('WILD');
+
+	})
+
+
+	test.each(melds)(
+		'test points', 
+		(rank, cards, valid, points) => {
+			var meld = new Meld(rank, cards);
+			expect(meld.validate()).toBe(valid);
+			expect(meld.points()).toBe(points);
+		}
+	)
+
+	test.todo('test other melds for points')
+	test.todo('test books for points')
 })
-
-test('less than three cards cant make a meld', () => {
-	var rank = Rank.FIVE;
-	var cards = [
-		new Card(Rank.FIVE, Suit.DIAMONDS), 
-		new Card(Rank.FIVE, Suit.HEARTS),
-	];
-	var meld = new Meld(rank, cards);
-	expect(meld.validate()).toBe(false);
-})
-
-test('three cards different suits cant make a meld', () => {
-	var rank = Rank.FIVE;
-	var cards = [
-		new Card(Rank.FIVE, Suit.HEARTS), 
-		new Card(Rank.FIVE, Suit.HEARTS),
-		new Card(Rank.SIX, Suit.HEARTS),
-	];
-	var meld = new Meld(rank, cards);
-	expect(meld.validate()).toBe(false);
-})
-
-test('adding cards to a created meld', () => {
-	var rank = Rank.FIVE;
-	var cards = [
-		new Card(Rank.FIVE, Suit.HEARTS), 
-		new Card(Rank.FIVE, Suit.DIAMONDS),
-		new Card(Rank.FIVE, Suit.HEARTS),
-	];
-	var meld = new Meld(rank, cards);
-	meld.addCard(new Card(Rank.FIVE, Suit.CLUBS));
-	expect(meld.validate()).toBe(true);
-	expect(meld.size()).toBe(4);
-})
-
-test('testing weird combinations of cards', () => {
-	var meld = new Meld(Rank.FIVE, [
-		new Card(Rank.FIVE, Suit.HEARTS),
-		new Card(Rank.FIVE, Suit.CLUBS),
-		new Card(Rank.TWO, Suit.SPADES)
-	])
-	expect(meld.validate()).toBe(true);
-
-	meld = new Meld(Rank.EIGHT, [
-		new Card(Rank.EIGHT, Suit.SPADES),
-		new Card(Rank.TWO, Suit.SPADES),
-		Card.RED_JOKER
-	])
-	expect(meld.validate()).toBe(false);
-
-	meld = new Meld(Rank.TWO, [
-		new Card(Rank.TWO, Suit.SPADES),
-		new Card(Rank.TWO, Suit.SPADES),
-		Card.BLACK_JOKER
-	])
-	expect(meld.validate()).toBe(true);
-
-	meld = new Meld(Rank.THREE, [
-		new Card(Rank.THREE, Suit.HEARTS)
-	])
-	expect(meld.validate()).toBe(true);
-
-	meld = new Meld(Rank.THREE, [
-		new Card(Rank.THREE, Suit.SPADES),
-		new Card(Rank.THREE, Suit.CLUBS),
-		new Card(Rank.THREE, Suit.SPADES)
-	])
-	expect(meld.validate()).toBe(false);
-
-	meld = new Meld(Rank.THREE, [
-		new Card(Rank.THREE, Suit.HEARTS),
-		new Card(Rank.THREE, Suit.HEARTS),
-		new Card(Rank.THREE, Suit.DIAMONDS)
-	])
-	expect(meld.validate()).toBe(true);
-
-	meld = new Meld(Rank.THREE, [
-		new Card(Rank.THREE, Suit.HEARTS),
-		new Card(Rank.THREE, Suit.HEARTS),
-		new Card(Rank.TWO, Suit.DIAMONDS)
-	])
-	expect(meld.validate()).toBe(false);
-})
-
-
-test('test isBook and getBookKind on red canasta', () => {
-	var meld = new Meld(Rank.FIVE, [
-		new Card(Rank.FIVE, Suit.HEARTS),
-		new Card(Rank.FIVE, Suit.DIAMONDS),
-		new Card(Rank.FIVE, Suit.CLUBS)
-	])
-	expect(meld.validate()).toBe(true);
-	expect(meld.isBook()).toBe(false);
-	expect(meld.getBookKind()).toBe(null);
-
-	meld.addCards([
-		new Card(Rank.FIVE, Suit.HEARTS),
-		new Card(Rank.FIVE, Suit.SPADES),
-		new Card(Rank.FIVE, Suit.DIAMONDS),
-		new Card(Rank.FIVE, Suit.HEARTS)
-	])
-	expect(meld.validate()).toBe(true);
-	expect(meld.isBook()).toBe(true);
-	expect(meld.getBookKind()).toBe('PURE');
-
-})
-
-test('test isBook and getBookKind on black canasta', () => {
-	var meld = new Meld(Rank.FIVE, [
-		new Card(Rank.FIVE, Suit.HEARTS),
-		new Card(Rank.FIVE, Suit.DIAMONDS),
-		new Card(Rank.FIVE, Suit.CLUBS),
-		Card.RED_JOKER,
-		new Card(Rank.FIVE, Suit.SPADES),
-		new Card(Rank.TWO, Suit.DIAMONDS),
-		new Card(Rank.FIVE, Suit.HEARTS)
-	])
-	expect(meld.validate()).toBe(true);
-	expect(meld.isBook()).toBe(true);
-	expect(meld.getBookKind()).toBe('DIRTY');
-
-})
-
-test('test isBook and getBookKind on wild canasta', () => {
-	var meld = new Meld(Rank.JOKER, [
-		new Card(Rank.TWO, Suit.HEARTS),
-		new Card(Rank.TWO, Suit.DIAMONDS),
-		new Card(Rank.TWO, Suit.CLUBS),
-		new Card(Rank.TWO, Suit.HEARTS),
-		Card.RED_JOKER,
-		Card.BLACK_JOKER,
-		Card.RED_JOKER
-	])
-	expect(meld.validate()).toBe(true);
-	expect(meld.isBook()).toBe(true);
-	expect(meld.getBookKind()).toBe('WILD');
-
-})
-
-
-test('test points on basic meld', () => {
-	var meld = new Meld(Rank.FIVE, [
-		new Card(Rank.FIVE, Suit.HEARTS),
-		new Card(Rank.FIVE, Suit.HEARTS),
-		new Card(Rank.FIVE, Suit.HEARTS),
-	])
-	expect(meld.validate()).toBe(true);
-	expect(meld.points()).toBe(15);
-})
-
-test.todo('test other melds for points')
-test.todo('test books for points')
